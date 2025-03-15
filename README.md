46f71fa8c0a71452795105debfe70b23152b32bb
Developer-WasimAkram <noorewasim@gmail.com>
2025-03-16 01:37:30 +0530

    workflow

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Medical_Chatbot
+---
+title: Medical Chatbot
+emoji: 🐢
+colorFrom: green
+colorTo: blue
+sdk: docker
+pinned: false
+---
+
+Check out the configuration reference at https://huggingface.co/docs/hub/spaces-config-reference
--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 from langchain_groq import ChatGroq
-
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
